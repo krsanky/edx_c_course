7ef33d4718016b4c87e1a6781ebc16dde97a738f
paul wisehart <paul@oldcode.org>
2019-07-01 03:29:35 +0000

    asd

---
+++ c1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int 
+main() 
+{
+    int i;
+
+    printf("+");
+    for (i = 0; i < 23; i++)
+        printf("-");
+    printf("+\n");
+
+    for (i = 0; i < 3; i++)
+        printf("| o | X | o | X | o | X |");
+        printf("\n");
+        printf("| X | o | X | o | X | o |");
+        printf("\n");
+
+    printf("+");
+    for (i = 0; i < 23; i++)
+        printf("-");
+    printf("+");
+
+    return(0);
+}
+
+
+
