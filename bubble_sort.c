0aa9d9f6497430f6b940d2be1fb2decba7c41f7a
paul wisehart <paul@oldcode.org>
2019-07-01 22:20:04 +0000

    asd

---
+++ bubble_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void
+print_arr(int arr[], int l)
+{
+	int	i;
+	for (i=0; i<l; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+}
+
+int
+main()
+{
+	int 		list      [] = {
+		759, 14, 2, 900, 106, 77, -10, 8, -3, 5
+	};
+	int 		n = 10;
+	int		i, swap;
+
+	print_arr(list, n);
+
+	for (i=0; i<n-1; i++) {
+		if (list[i] > list[i+1]) {
+			swap = list[i];
+			list[i] = list[i+1];
+			list[i+1] = swap;
+		}
+	}
+
+	print_arr(list, n);
+
+	return 0;
+}
