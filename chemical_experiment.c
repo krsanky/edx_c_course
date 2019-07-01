3b5001e1ce3fe3df26ecf820d75803599782eddd
paul wisehart <paul@oldcode.org>
2019-07-01 20:08:44 +0000

    asd

--- chemical_experiment.c
+++ chemical_experiment.c
@@ -6,8 +6,10 @@ main()
 	int	min, max, t;
 	scanf("%d%d", &min, &max);
 	t = 0;
-	while (t != -999) {
+	while (1) {
 		scanf("%d", &t);
+		if (t == -999)
+			break;
 		if ((t < min) || (t > max)) {
 			printf("Alert!\n");
 			break;
