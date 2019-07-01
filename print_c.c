2f97c76d74bbecea782f4242de997b4d36748a80
paul wisehart <paul@oldcode.org>
2019-07-01 14:45:16 +0000

    asd

---
+++ print_c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	//Programming in C
+	char	letter1, letter2, letter3;
+	letter1 = 'i';
+	letter2 = 'n';
+	letter3 = 'C';
+	letter1 = 'i';
+	printf ("Programming %c%c %c\n", letter1, letter2, letter3);
+
+	return 0;
+}
