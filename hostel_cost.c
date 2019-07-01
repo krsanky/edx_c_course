0974879af6112b7544de2196a9f8a350b376efb0
paul wisehart <paul@oldcode.org>
2019-07-01 18:31:21 +0000

    asd

--- hostel_cost.c
+++ hostel_cost.c
@@ -2,7 +2,21 @@
 
 int main()
 {
+	int	age, wt, cost;
+	scanf("%d%d", &age, &wt);
 
+	if (age == 60) 
+		cost = 0;
+	else if (age < 10)
+		cost = 5;
+	else {
+		cost = 30;
+		if (wt > 20) {
+			cost += 10;
+		}
+	}
+	printf("%d\n", cost);
+	
 	return 0;
 }
 
