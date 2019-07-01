3fee17928aa2239ddfea0354e744caecdd8d5613
paul wisehart <paul@oldcode.org>
2019-07-01 18:07:59 +0000

    asd

---
+++ bridge_tax.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+	int	n1, n2, sum, fee;
+
+	scanf("%d%d", &n1, &n2);
+
+	sum = n1 + n2;
+	if (sum >= 10) {
+		printf("Special tax\n36\n");
+	} else {
+		printf("Regular tax\n%d\n", 2*sum);
+	}
+	return 0;	
+}
