a55e1fd10182a777ee128424477426163bc90b31
paul wisehart <paul@oldcode.org>
2019-07-03 03:55:01 +0000

    asd

--- toaster.c
+++ toaster.c
@@ -5,8 +5,29 @@ main()
 {
 	int	amt;
 	char	type;
+	int	per;
+	int	total;
 	scanf(" %c %d", &type, &amt);		
 	printf("type:%c amt:%d\n", type, amt);		
+
+	switch (type) {
+	case 'i':
+		per = sizeof(int);
+		break;
+	case 's':
+		per = sizeof(short);
+		break;
+	case 'c':
+		per = sizeof(char);
+		break;
+	case 'd':
+		per = sizeof(double);
+		break;
+	}
+	printf("per:%d\n", per);
+	total = amt * per;
+	printf("total:%d\n", total);
+
 	return 0;
 }
 
