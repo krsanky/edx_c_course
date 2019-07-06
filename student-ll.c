19792f2efbc72be650a24fe11d14894bc647e8e0
paul wisehart <paul@oldcode.org>
2019-07-06 16:09:58 +0000

    indent

--- student-ll.c
+++ student-ll.c
@@ -3,23 +3,23 @@
 #include <string.h>
 
 struct student {
-	char	name[50];
-	int	age;
-	struct student	*next;
+	char 		name     [50];
+	int 		age;
+	struct student *next;
 };
 
 int
 main()
 {
-	struct student	*s1, *s2;
-	s1 = (struct student *)malloc(sizeof(struct student));
-	s2 = (struct student *)malloc(sizeof(struct student));
+	struct student *s1, *s2;
+	s1 = (struct student *) malloc(sizeof(struct student));
+	s2 = (struct student *) malloc(sizeof(struct student));
 
 	strcpy(s1->name, "asdasd");
 	s1->age = 12;
 	printf("%s is %d years old.\n", s1->name, s1->age);
 
-	
+
 	strcpy(s2->name, "22asdasd");
 	s2->age = 42;
 	s1->next = s2;
@@ -29,4 +29,3 @@ main()
 	free(s2);
 	return 0;
 }
-
