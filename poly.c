1cd6fa71adf4e9c9e27030e019631224fd1e59d0
paul wisehart <paul@oldcode.org>
2019-07-06 04:18:24 +0000

    indent

--- poly.c
+++ poly.c
@@ -1,37 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct point{
-	int x;
-	int y;
+struct point {
+	int 		x;
+	int 		y;
 };
 
-void printPoint(struct point);
-void printPoly(struct point *, int);
-void initializePoly(struct point *, int);
+void 		printPoint(struct point);
+void 		printPoly(struct point *, int);
+void 		initializePoly(struct point *, int);
 
-int main(void) {
-	int N;    
+int 
+main(void)
+{
+	int 		N;
 	scanf("%d", &N);
-    // Fill in your main function here
+	//Fill in your main function here
 
 }
 
-void printPoint(struct point pt) {
-    printf("(%d, %d)\n", pt.x, pt.y);
-}
-
-void printPoly(struct point *ptr, int N) {
-    int i;
-    for (i=0; i<N; i++) {
-        printPoint(ptr[i]);
-    }
-}
-
-// Write your initializePoly() function here
 void 
-initializePoly(struct point * ptr, int N)
+printPoint(struct point pt)
+{
+	printf("(%d, %d)\n", pt.x, pt.y);
+}
+
+void 
+printPoly(struct point * ptr, int N)
+{
+	int 		i;
+	for (i = 0; i < N; i++) {
+		printPoint(ptr[i]);
+	}
+}
+
+//Write your initializePoly()
+	function 	here
+	void
+			initializePoly(struct point * ptr, int N)
 {
 
 }
-
