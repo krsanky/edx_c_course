4b2a18b840397d464ed0da9c4a41cf07dd66e1f7
paul wisehart <paul@oldcode.org>
2019-07-06 01:28:00 +0000

    asd

---
+++ README.md
@@ -0,0 +1,5 @@
+The syntax is not ideal, but not worth fixing.
+The course uses a web text editor that wants 4-space non tab indents.
+I want to indent with OpenBSD's indent program that uses the classic 
+8 space tabs.
+ 
---
+++ adv-date.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct date {
+        int year;
+        int month;
+        int day;
+    };
+
+void printDate(struct date);
+void readDate(struct date *);
+
+struct date advanceDay(struct date); 
+
+int monthMax(int);
+
+int main(void) {
+	struct date today, tomorrow;
+	readDate(&today);
+	printDate(today);
+	tomorrow = advanceDay(today);
+	printDate(tomorrow);
+	return 0;
+}
+
+// Write your readDate(), printDate(), and advanceDate() functions here
+int
+monthMax(int m)
+{
+	switch (m) {
+	case 1: return 31;
+	case 2: return 28;
+	case 3: return 31;
+	case 4: return 30;
+	case 5: return 31;
+	case 6: return 30; 
+	case 7: return 31; 
+	case 8: return 31; 
+	case 9: return 30; 
+	case 10: return 31; 
+	case 11: return 30; 
+	case 12: return 31; 
+	}
+	return -1;
+}
+
+struct date 
+advanceDay(struct date d) {
+	struct date * newd;
+	newd = (struct date *)malloc(sizeof(struct date));
+	newd->year = d.year;
+	newd->day = d.day;
+	newd->month = d.month;
+	return *newd;    
+}
+
+void readDate(struct date * d)
+{
+    scanf("%d%d%d", &d->year, &d->month, &d->day);
+}
+
+void printDate(struct date d)
+{
+    printf("%d/%02d/%d\n", d.month, d.day, d.year);
+    
+}
+
+
