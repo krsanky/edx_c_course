5f3c1938e9aaa167ad02373420725a87dea25c65
paul wisehart <paul@oldcode.org>
2019-07-07 14:12:48 +0000

    asd

--- plants.c
+++ plants.c
@@ -5,9 +5,17 @@
 int
 main(int argc, char **argv)
 {
+	int	nump;
+	double	price;
+	
 	if (argc != 3) {
 		printf("Invalid input.\n");
 		exit(1);
 	}
+	nump = atoi(argv[1]);
+	price = atof(argv[2]);
+	/*scanf("%d%lf", &nump, &price);*/
+
+	printf("%d plants for %.2lf dollars each cost %.2lf dollars.\n", nump, price, nump * price);
 	return 0;
 }
