45dc0d01844c0850b33071fef959a3a396823c13
paul wisehart <paul@oldcode.org>
2019-07-07 15:05:57 +0000

    asd

--- append-grade.c
+++ append-grade.c
@@ -5,8 +5,26 @@ main()
 {
 	FILE	*f;
 	char	fn[] = "myGrades.txt";
+	int	gr, check_gr;
 
+	scanf("%d", &check_gr);
 	f = fopen(fn, "r");
+	while (fscanf(f, "%d", &gr) != EOF) {
+		//printf("gr:%d %d\n", gr, check_gr);
+	}
+	fclose(f);
+
+	if (gr != check_gr) {
+		f = fopen(fn, "a");
+		fprintf(f, "%d", check_gr);
+		fclose(f);
+	}
+	
+	f = fopen(fn, "r");
+	while (fscanf(f, "%d", &gr) != EOF) {
+		printf("%d ", gr);
+	}
+	printf("\n");
 	fclose(f);
 
 	return 0;
